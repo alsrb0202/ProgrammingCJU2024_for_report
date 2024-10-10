07cd79c5fc7fe196a0c3498117e9f0ea47ca33a7
alsrb0202 <alsrb020202@gmail.com>
2024-10-10 22:03:12 +0900

    2021010508 이민규 과제

--- report 1/report/FileName.cpp
+++ report 1/report/FileName.cpp
@@ -8,10 +8,9 @@ int main(void)
     printf("항공기의 각도 입력 : ");
     scanf_s("%d", &angle);
 
+    angle = (angle >= 0) * (angle - (angle / 360) * 360) + (angle < 0) * (angle + 360);//각도를 0도에서 360로 범위를 변환하는 수식입니다
 
-    res = (angle >= 0 && angle <= 180); //각도가 0~180도 사이면 그대로 표출
-    res = (angle > 180);
-    res = (angle - 360);//각도가 180이상이면 -360후 각도 표출
+    res = (angle >= 0 && angle <= 180) * angle + (angle > 180) * (angle - 360);//각도가 180도를 넘어갔을때 음수영역으로 바꾸어주는 수식입니다
 
     printf("항공기의 각도는 %d도\n", res);
 
